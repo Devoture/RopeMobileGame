2486661364eafe2a4d49b72ab7f7505f81516ec5
Josh Brackett <joshua.brackett@hotmail.ca>
2018-08-05 17:37:51 -0400

    arm rotation works. Starting Rope now

--- Assets/JoshSandbox/Scripts/ArmRotation.cs
+++ Assets/JoshSandbox/Scripts/ArmRotation.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class ArmRotation : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update() {
+		if(Input.GetMouseButton(0)) {
+			// convert mouse position into world coordinates
+			Vector3 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Vector3 difference = mouseScreenPosition - transform.position;
+			float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+		}
 	}
 }
