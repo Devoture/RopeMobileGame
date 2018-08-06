497e921252c088221645daacfbde44f7814654b4
Josh Brackett <joshua.brackett@hotmail.ca>
2018-08-06 12:35:00 -0400

    added a gamemanager that tracks the score and added the lava so it accels over time and if it touches the player it calls game over and the score stops

---
+++ Assets/JoshSandbox/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager Instance { get { return instance; }}
+	public Text scoreText;
+	private bool gameOver;
+
+	private static GameManager instance;
+	private int score = 0;
+
+	void Awake() {
+		instance = this;
+	}
+
+	void Start() {	
+		StartCoroutine(UpdateScore());
+	}
+
+	IEnumerator UpdateScore() {
+		yield return new WaitForSeconds(1);
+		if(!gameOver) {
+			score += 10;
+			UpdateHUD();
+			StartCoroutine(UpdateScore());
+		}
+	}
+
+	void UpdateHUD() {
+		scoreText.text = "Score: " + score.ToString();
+	}
+
+	public void GameOver() {
+		gameOver = true;
+		Debug.Log("Game Over");
+	}
+}
---
+++ Assets/JoshSandbox/Scripts/Lava.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lava : MonoBehaviour {
+
+	public float speed = 5.0f;
+	public float acceleration = 1.0f;
+	public float maxSpeed;
+
+	void Update() {
+		if(speed < maxSpeed) {
+			speed += Time.deltaTime * acceleration;
+		} 
+		if(speed > maxSpeed) {
+			speed = maxSpeed;
+		}
+		transform.Translate(Vector2.up * speed);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.gameObject.tag == "Player") {
+			GameManager.Instance.GameOver();
+		}
+	}
+}
