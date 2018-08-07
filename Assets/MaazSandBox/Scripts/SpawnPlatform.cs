58c20a0aa14c2f1962221e284ec2f33f80c88ac4
Josh Brackett <joshua.brackett@hotmail.ca>
2018-08-06 22:17:42 -0400

    moving platform fully works and will randomy spawn the static or moving ones

--- Assets/MaazSandBox/Scripts/SpawnPlatform.cs
+++ Assets/MaazSandBox/Scripts/SpawnPlatform.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnPlatform : MonoBehaviour {
-	public GameObject platformPrefab;
+	public GameObject[] platformPrefabs;
 
 	private float platformSize = 7.5f;
-	
+
 	void Start() {
 		Spawn();
 	}
 
 	public void Spawn() {
+		int random = Random.Range(0, platformPrefabs.Length);
 		Vector3 pos = Vector3.zero;
 		pos = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(platformSize, Screen.width - platformSize), (Screen.height * 0.75f), 10));
-		Instantiate(platformPrefab, pos, Quaternion.identity);
+		Instantiate(platformPrefabs[random], pos, Quaternion.identity);
 	}
 }
