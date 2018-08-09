88c211bbfecbcacf259582da5c0a63f845062990
MaximumCarnage <dylansbruton@gmail.com>
2018-08-08 22:45:58 -0400

    added menu system, will need to fix scaling

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+
+	public void PlayGame(){
+		SceneManager.LoadScene("Main");
+	}
+	public void QuitGame(){
+		Application.Quit();
+	}
+}
